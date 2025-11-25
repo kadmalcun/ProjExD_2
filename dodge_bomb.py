66c4e1b1e53c2552c2728ac57fcc20158b23429d
kaede <c0a24205f3@edu.teu.ac.jp>
2025-11-25 14:25:55 +0900

    練習2

---
+++ dodge_bomb.py
@@ -0,0 +1,74 @@
+import os
+import sys
+import random
+import pygame as pg
+
+
+WIDTH, HEIGHT = 1100, 650
+DELTA = {
+    pg.K_UP: (0, -5),
+    pg.K_DOWN: (0, +5),
+    pg.K_LEFT: (-5, 0),
+    pg.K_RIGHT: (+5, 0),
+}
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+def check_bound(rct):
+    yoko, tate = True, True
+    if rct.left < 0 or WIDTH <rct.right:
+        yoko = False
+    if rct.top < 0 or HEIGHT < rct.bottom:
+        tate = False
+    return yoko, tate
+
+def main():
+    pg.display.set_caption("逃げろ！こうかとん")
+    screen = pg.display.set_mode((WIDTH, HEIGHT))
+    bg_img = pg.image.load("fig/pg_bg.jpg")    
+    kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
+    kk_rct = kk_img.get_rect()
+    kk_rct.center = 300, 200
+    bb_img = pg.Surface((20, 20)) #空のSurface
+    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10) #赤い円を描く
+    bb_rct = kk_img.get_rect() #爆弾Rect
+    bb_rct.centerx = random.randint(0, WIDTH) #爆弾x座標
+    bb_rct.centery = random.randint(0, HEIGHT) #爆弾y座標
+    vx,vy = +5, +5 #爆弾のx,y速度
+    
+    clock = pg.time.Clock()
+    tmr = 0
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT: 
+                return
+        screen.blit(bg_img, [0, 0]) 
+
+        key_lst = pg.key.get_pressed()
+        sum_mv = [0, 0]
+        # if key_lst[pg.K_UP]:
+        #     sum_mv[1] -= 5
+        # if key_lst[pg.K_DOWN]:
+        #     sum_mv[1] += 5
+        # if key_lst[pg.K_LEFT]:
+        #     sum_mv[0] -= 5
+        # if key_lst[pg.K_RIGHT]:
+        #     sum_mv[0] += 5
+        for key, mv in DELTA.items():
+            if key_lst[key]:
+                sum_mv[0] += mv[0]
+                sum_mv[1] += mv[1] 
+                
+        kk_rct.move_ip(sum_mv)
+        screen.blit(kk_img, kk_rct)
+        bb_rct.move_ip(vx, vy)
+        screen.blit(bb_img, bb_rct)
+        pg.display.update()
+        tmr += 1
+        clock.tick(50)
+
+
+if __name__ == "__main__":
+    pg.init()
+    main()
+    pg.quit()
+    sys.exit()
